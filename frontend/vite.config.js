c2f96505d12ad4b3757d48fcd18fc345ca067423
Mohitbhowal09 <97435506+Mohitbhowal09@users.noreply.github.com>
2025-10-23 23:45:44 +0530

    Remove unnecessary comments in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,7 +9,5 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist'
-  },
-  // Tell Vite where to find index.html
-  root: '.',  // This tells Vite to look in the current directory
+  }
 })
